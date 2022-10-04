17c09ebbc784fb7de79dd25e3b7a7a3e8039e1a8
TaylonSantos171 <114676185+TaylonSantos171@users.noreply.github.com>
2022-10-04 13:03:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,8 @@
-X = 0
-Y = 0
-Z = 0
-N = 0
-stop = False
-while stop == False :
-    print("Candidato X => 889")
-    print("Candidato Y => 847")
-    print("Candidato Z => 515")
-    print("Nulo/Branco => 0")
-    try:
-     voto = int(input("Qual seu voto"))
-     
-     def eleicao(voto) :
-      if voto ==889: resu = 1
-      elif voto ==847: resu = 2
-      elif voto ==515: resu = 3 
-      else : resu = 0 
-      return resu
-      
-     apu = eleicao(voto) 
-     if apu == 1:
-      X = X + 1
-     if apu == 2:
-      Y = Y + 1
-     if apu == 3:
-      Z = Z + 1
-     if apu == 0:
-      N = N + 1
-      
-      stop1 = False
-      while stop1 == False :
-        confirma = str (input("deseja encerrar a votação? S/N"))
-       if (confirma == "S"): stop = True
-           stop1 = True
-       if (confirma == "N"):
-           stop = False
-           stop1 = True
-      else : print ("responda denovo por favor")
-          
-    except :
-        print("nao compreendi")
-print ("voce encerrou a votaçao")
-if(X>Y) and (X>Z) : print("O eleito é o X")
-if(X<Y) and (Y>Z) : print("O eleito é o Y")
-if(X<Z) and (Y<Z) : print("O eleito é o Z")
-else : print("empate")
-print ("o candidato X teve:",X)  
-print ("o candidato Y teve:",Y)  
-print ("o candidato Z teve:",Z)  
-print ("Teve ", N," votos em branco")  
+import pandas as pd
+df = pd.read_cvs("notas_alunos.csv")
+df = df.set_index('Aluno')
+media_aluno = ((df["Notas_1"] + df["Notas_2"])/2)
+df['Media_Aluno'] = Media_Aluno
+df.loc[df['Media_Aluno'] <= 7,"Situação"] or [df['Faltas'] >= 5] = 'Reprovado'
+df.loc[df['Media_Aluno'] >= 7,"Situação"] or [df['Faltas'] < 5] = 'Aprovado'
+Mais_Faltas
