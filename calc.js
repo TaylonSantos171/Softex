e07b338b42eff3d881ece1fffffd1be4f65af664
TaylonSantos171 <114676185+TaylonSantos171@users.noreply.github.com>
2022-10-20 21:18:46 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+
+var n1 = 10;
+var n2 = 2;
+var ope = "+";
+var result;
+
+if (ope === "+") // se o ope for igual a + entao vai fazer a adição do n1 pelo n2
+ result = n1 + n2 
+
+else if (ope === "-") // se o ope for igual a - entao vai fazer a subtração do n1 pelo n2
+ result = n1 - n2
+ 
+ else if (ope === "*") // se o ope for igual a * entao vai fazer a mutiplição do n1 pelo n2
+ result = n1 * n2
+
+ else if (ope === "/") // se o ope for igual a / entao vai fazer a divisao do n1 pelo n2 deixando numero com virgula
+ result = n1 / n2
+
+ else if (ope === "%") // se o ope for igual a % entao vai fazer o modulo de divisão do n1 pelo n2 e retorna o resto
+ result = n1 % n2
