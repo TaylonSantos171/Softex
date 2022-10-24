697912b38c3bef9f75106ef0784aba76f77dd38a
TaylonSantos171 <114676185+TaylonSantos171@users.noreply.github.com>
2022-10-24 19:59:33 -0300

    Delete notasalunos2.js

--- notasalunos2.js
+++
@@ -1,8 +0,0 @@
-var nota1;
-var nota2;
-var media;
-
-media = (nota1 + nota2)
-medianecessaria = 21 - media;
-
-var result = media >= 21 ? alert("você ja ta com media 7") : alert("Voce precisa tirar: "+medianecessaria);
