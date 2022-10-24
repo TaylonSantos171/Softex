ab4ec91aa8b4f37eb5af14cfffd2d8a9f600ff5c
TaylonSantos171 <114676185+TaylonSantos171@users.noreply.github.com>
2022-10-24 20:00:51 -0300

    Add files via upload

---
+++ Atividade Notas alunos/notasalunos1.js
@@ -0,0 +1,8 @@
+var nota1;
+var nota2;
+var nota3;
+var media;
+
+media = (nota1 + nota2 + nota3)/3;
+
+media >= 7 ? alert("você foi aprovado") : alert("voce foi reprovado");
---
+++ Atividade Notas alunos/notasalunos2.js
@@ -0,0 +1,8 @@
+var nota1;
+var nota2;
+var media;
+
+media = (nota1 + nota2)
+medianecessaria = 21 - media;
+
+var result = media >= 21 ? alert("você ja ta com media 7") : alert("Voce precisa tirar: "+medianecessaria);
