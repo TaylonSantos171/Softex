5973cf54502fc6c78b2329a2ca47cd9319c22815
TaylonSantos171 <114676185+TaylonSantos171@users.noreply.github.com>
2022-12-01 08:58:25 -0300

    Add files via upload

---
+++ react alterado/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola React
+        meu nome é Tiago Taylon e estou cursando na Softex
+      </h1>
+    </div>
+  );
+}
+
+export default App;
